4b8f3a4df4d109e573461151ab8cd988f5fbf6e7
Amotz <amotzte@gmail.com>
2016-12-03 15:50:10 +0200

    Check if user have mock location enable

--- app/src/main/java/amotz/example/com/mocklocationfordeveloper/MainActivity.java
+++ app/src/main/java/amotz/example/com/mocklocationfordeveloper/MainActivity.java
@@ -1,5 +1,10 @@
 package amotz.example.com.mocklocationfordeveloper;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (!isMockSettingsON(this)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("In order to use this app you must enable mock location do you want to enable it now?").setTitle("Mock location is not enable");
+            builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Intent i =new Intent(android.provider.Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
+                    startActivity(i);
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    finish();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
+    }
+
+    private boolean isMockSettingsON(Context context) {
+        String st=Settings.Secure.getString(context.getContentResolver(),Settings.Secure.ALLOW_MOCK_LOCATION);
+        return st.equals("1");
     }
 }
---
+++ app/src/main/java/amotz/example/com/mocklocationfordeveloper/adbBrodcastReceiver.java
@@ -0,0 +1,10 @@
+package amotz.example.com.mocklocationfordeveloper;
+
+import BrodcastReceiver;
+
+/**
+ * Created by amotz on 12/3/2016.
+ */
+
+public class adbBrodcastReceiver extends BrodcastReceiver {
+}
